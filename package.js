5205871be9a7fac496aba7d0fb1ea05b36361787
chandonnet <franki_football@hotmail.com>
2021-11-02 13:37:49 -0400

    meteor 2.3 compat

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
-  name: 'vatfree:restivus',
+  name: 'fchandonnet:restivus',
   summary: 'Create authenticated REST APIs in Meteor via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
-  version: '1.1.0',
-  git: 'https://github.com/vatfree/meteor-restivus.git'
+  version: '1.1.1',
+  git: 'https://github.com/chandonnet/meteor-restivus'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.10');
+  api.versionsFrom('2.3');
 
   // Meteor dependencies
   api.use('ecmascript');
   api.use('check');
   api.use('underscore');
-  api.use('accounts-password@1.6.2');
-  api.use('simple:json-routes@2.1.0');
-  api.use('alanning:roles@1.2.19', 'server', {weak: true});
+  api.use('accounts-password');
+  api.use('simple:json-routes');
+  api.use('alanning:roles', 'server', {weak: true});
 
   api.addFiles([
     'lib/auth.js',
